5f5b4285dc67757a2bf1eeacc06f438a1163c8c3
wasabeef <dadadada.chop@gmail.com>
2020-09-15 23:16:36 +0900

    format

--- richeditor/src/test/java/richeditor/UrlDecoderTest.java
+++ richeditor/src/test/java/richeditor/UrlDecoderTest.java
@@ -10,14 +10,14 @@ import org.robolectric.RobolectricTestRunner;
 @RunWith(RobolectricTestRunner.class)
 public class UrlDecoderTest {
 
-    @Test
-    public void urlDecodeTest() throws Exception {
-        String encoded = "re-callback://%20%20%3Cdiv%3E%3Cfont%20face=%22Arial%22%20size=%222%22%20color=%22#333333%22%3EDJK%20Besteck%3C/font%3E%3Cb%3E%3Cfont%20face=%22Arial%22%20size=%222%22%20color=%22#333333%22%3E&nbsp;Kenntnisse++_%20++ 下 ぁ ص%3C/font%3E%3C/b%3E%3C/div%3E%20";
-        String decoded = Uri.decode(encoded);
+  @Test
+  public void urlDecodeTest() throws Exception {
+    String encoded = "re-callback://%20%20%3Cdiv%3E%3Cfont%20face=%22Arial%22%20size=%222%22%20color=%22#333333%22%3EDJK%20Besteck%3C/font%3E%3Cb%3E%3Cfont%20face=%22Arial%22%20size=%222%22%20color=%22#333333%22%3E&nbsp;Kenntnisse++_%20++ 下 ぁ ص%3C/font%3E%3C/b%3E%3C/div%3E%20";
+    String decoded = Uri.decode(encoded);
 
-        String test = "re-callback://  <div><font face=\"Arial\" size=\"2\" color=\"#333333\">DJK Besteck</font><b><font face=\"Arial\" size=\"2\" color=\"#333333\">&nbsp;Kenntnisse++_ ++ 下 ぁ ص</font></b></div> ";
+    String test = "re-callback://  <div><font face=\"Arial\" size=\"2\" color=\"#333333\">DJK Besteck</font><b><font face=\"Arial\" size=\"2\" color=\"#333333\">&nbsp;Kenntnisse++_ ++ 下 ぁ ص</font></b></div> ";
 
-        Assert.assertEquals(test, decoded);
-    }
+    Assert.assertEquals(test, decoded);
+  }
 
 }
